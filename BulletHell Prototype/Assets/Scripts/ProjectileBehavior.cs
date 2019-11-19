86785901831498d6d71703f2e98f38f0316009b5
Rodrigo Porto <mportorodrigo@outlook.com>
2019-11-19 18:14:59 -0300

    Update ProjectileBehavior.cs
    Destroy game object after few seconds

--- BulletHell Prototype/Assets/Scripts/ProjectileBehavior.cs
+++ BulletHell Prototype/Assets/Scripts/ProjectileBehavior.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class ProjectileBehavior : MonoBehaviour
 {
-    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float speed = 30.0f;
+    [SerializeField] private int lifeSpan = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Destroy the projectile after a few seconds
+        Destroy(gameObject, lifeSpan);
+    }
 
     // Update is called once per frame
     void Update()
     {
+        // Moves the projectile
         transform.Translate(Vector3.up * Time.deltaTime * speed);
     }
 }
