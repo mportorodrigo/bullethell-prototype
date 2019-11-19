e4227931e07f86e8825ef4a39f25b73954e2e0c7
Rodrigo Porto <mportorodrigo@outlook.com>
2019-11-19 16:57:45 -0300

    Add PlayerController

---
+++ BulletHell Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 10.0f;
+    float horizontalInput;
+    float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        transform.Translate(Vector3.up * Time.deltaTime * verticalInput * speed);
+    }
+}
