9e1d2b276353d42644c8263f5f26e9dea380fd46
Rodrigo Porto <mportorodrigo@outlook.com>
2019-11-19 18:01:19 -0300

    Fix script name typo
    Changed projectileBehavior to PorjectileBehavior

---
+++ BulletHell Prototype/Assets/Scripts/ProjectileBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour
+{
+    [SerializeField] private float speed = 20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+}
