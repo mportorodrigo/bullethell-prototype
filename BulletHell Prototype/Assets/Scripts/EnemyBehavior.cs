1be22c8338d1396ad3317232824445154e5bff32
Rodrigo Porto <mportorodrigo@outlook.com>
2019-11-22 16:27:37 -0300

    Add EnemyBehavior

---
+++ BulletHell Prototype/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float fireRate = 1.0f;
+    private float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextFire = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the enemy ship
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Time.time > nextFire)
+        {
+            Instantiate(projectile, transform.position, transform.rotation);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
