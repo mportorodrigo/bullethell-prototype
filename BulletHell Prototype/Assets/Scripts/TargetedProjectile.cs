8071c6f7420437e57b2ae88fd24a3735d95eb4bd
Rodrigo Porto <mportorodrigo@outlook.com>
2019-11-22 16:59:33 -0300

    Add TargetedProjectile

---
+++ BulletHell Prototype/Assets/Scripts/TargetedProjectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetedProjectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    private GameObject target;
+    private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player");
+        moveDirection = (transform.position - target.transform.position).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveDirection * Time.deltaTime * speed);
+    }
+}
